b94e1376b77a49f521dab07db1d75f3b3af8f580
Sviat <102855447+kokos220@users.noreply.github.com>
2025-11-29 10:39:07 +0200

    Add files via upload
    Повністю робочий код, тільки треба покращити читабельність

--- computer_project.py
+++ computer_project.py
@@ -3,22 +3,28 @@ def read_file(file_name: str) -> list[str]:
         file_con = file.readlines()
     return file_con
 
-def create_dictionaries(file_content: list[str]) -> tuple[dict, dict]:
+def create_dictionaries(file_content: list[str]) -> tuple:
     vertical_out = {}
     vertical_in = {}
     page_rank = {}
     vertexes = set()
     not_used_vertexes = set()
+    end_point = ''
     for elem in file_content[1:-1]:
         elem = elem.replace('\n', '').replace(' ', '')
         if '->' in elem:
             start_point, end_point = elem.split('->')
         else:
             start_point = elem.strip()
+        if not start_point:
+            continue
         if end_point and start_point:
             vertical_out.setdefault(start_point, set()).add(end_point)
             vertical_in.setdefault(end_point, set()).add(start_point)
-        vertexes.update(start_point, end_point)
+        if start_point:
+            vertexes.add(start_point)
+        if end_point:
+            vertexes.add(end_point)
         start_point, end_point = '', ''
     for point in vertexes:
         if point not in vertical_out and point not in vertical_in:
@@ -27,32 +33,25 @@ def create_dictionaries(file_content: list[str]) -> tuple[dict, dict]:
         vertexes.add(start_point)
     if end_point:
         vertexes.add(end_point)
-    page_rank = page_rank.fromkeys(vertexes, 1/(len(vertexes) + len(not_used_vertexes)))
-    page_rank.update(page_rank.fromkeys(not_used_vertexes, (1 - 0.85)/(len(vertexes) + len(not_used_vertexes))))
+    page_rank = page_rank.fromkeys(vertexes, 1/len(vertexes))
     page_rank = dict(sorted(page_rank.items(), key=lambda x: x[0]))
-
-    # print(f'{vertical_out} - вихід: вхід')
-    # print(f'{vertical_in} - вхід: вихід')
-    # print(f'{sorted(vertexes)} - вершини звязні')
-    # print(f'{sorted(not_used_vertexes)} - вершини ізольовані')
-    # print(page_rank)
-
     return vertical_in, vertical_out, page_rank, vertexes, not_used_vertexes
 
 def get_page_rank(page_rank, out_in_ribs, in_out_ribs, peaks, useless_peaks):
-    all_vertexes_counter = len(useless_peaks) + len(peaks)
+    all_vertexes_counter = len(peaks)
     previous_pr = page_rank.copy()
+    all_peaks = peaks | useless_peaks
     start = True
     no_exit_vert = [vert for vert in peaks if vert not in out_in_ribs]
     while start or any(abs(previous_pr[value] - page_rank[value]) > 1e-6 for value in page_rank):
         start = False
         previous_pr = page_rank.copy()
         dangling_sum = sum(previous_pr[v] for v in no_exit_vert) ## cont S = sum(prev[v] for v in dangling)
-        for peak in peaks:
+        for peak in all_peaks:
             coefficients_sum = 0
-            if peak in in_out_ribs.keys(): #Якщо вершина має вхід
-                for _, point in enumerate(in_out_ribs[peak]):
-                    point_rank = page_rank[point]
+            if peak in in_out_ribs: #Якщо вершина має вхід
+                for point in in_out_ribs[peak]:
+                    point_rank = previous_pr[point]
                     point_outs = len(out_in_ribs[point])
                     coefficients_sum += point_rank/point_outs
             
